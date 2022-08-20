4059a81db38e14fb7b5c197bdd81471d932088b5
Quoc Tan <darknight1604@gmail.com>
2022-08-20 22:57:54 +0700

    Refactor route

--- lib/core/helpers/logi_route.dart
+++ lib/core/helpers/logi_route.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:logi/screens/fruit_screen/presentation/fruit_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logi/core/applications/authorization/authorization_bloc.dart';
+import 'package:logi/screens/home_screen/presentations/home_screen.dart';
 import 'package:logi/screens/not_found/presentations/not_found_screen.dart';
 import 'package:logi/screens/welcome_screen/presentations/welcome_screen.dart';
 
 class LogiRoute {
-  static const String fruits = '/fruits';
-
   static const String welcomeScreen = '/welcomeScreen';
+
+  static const String homeScreen = '/homeScreen';
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case fruits:
-        return MaterialPageRoute(
-          builder: (context) {
-            return const FruitScreen();
-          },
-        );
       case welcomeScreen:
         return MaterialPageRoute(
           builder: (context) {
+            AuthorizationState authorizationState =
+                BlocProvider.of<AuthorizationBloc>(context).state;
+            if (authorizationState is UserAuthorizedState &&
+                authorizationState.isAuthorized) {
+              return const HomeScreen();
+            }
             return const WelcomeScreen();
+          },
+        );
+      case homeScreen:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const HomeScreen();
           },
         );
       default:
