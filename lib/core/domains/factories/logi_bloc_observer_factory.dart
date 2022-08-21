795dc8040dcf35cd42e01b71e7c042678e5fecec
Quoc Tan <darknight1604@gmail.com>
2022-08-21 11:57:42 +0700

    Refactor directory observer

--- lib/core/domains/factories/logi_bloc_observer_factory.dart
+++ lib/core/domains/factories/logi_bloc_observer_factory.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:logi/core/helpers/default_bloc_observer.dart';
-import 'package:logi/core/helpers/logi_bloc_observer.dart';
+import 'package:logi/core/infastructures/observers/default_bloc_observer.dart';
+import 'package:logi/core/infastructures/observers/logi_bloc_observer.dart';
 
 class LogiBlocObserverFactory {
   static BlocObserver getBlocObserver({bool defaultObserver = false}) {
