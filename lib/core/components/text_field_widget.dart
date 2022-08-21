db2a130f70ef27138a51632b1d2e9c587f6e06db
Quoc Tan <darknight1604@gmail.com>
2022-08-21 20:05:09 +0700

    Update components

--- lib/core/components/text_field_widget.dart
+++ lib/core/components/text_field_widget.dart
@@ -5,22 +5,34 @@ import 'package:logi/core/helpers/text_style_manager.dart';
 
 class TextFieldWidget extends StatelessWidget {
   final String? hintText;
+  final String? labelText;
   final TextEditingController? controller;
   final void Function(String value)? onSubmitted;
+  final TextStyle? labelStyle;
+  final TextStyle? hintStyle;
+  final FocusNode? focusNode;
+
   const TextFieldWidget({
     Key? key,
     this.hintText,
     this.controller,
     this.onSubmitted,
+    this.labelStyle = TextStyleManager.smallText,
+    this.labelText,
+    this.hintStyle = TextStyleManager.normalText,
+    this.focusNode,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      focusNode: focusNode,
       controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
-        hintStyle: TextStyleManager.normalText,
+        hintStyle: hintStyle,
+        labelText: labelText,
+        labelStyle: labelStyle,
       ),
       onSubmitted: onSubmitted,
     );
