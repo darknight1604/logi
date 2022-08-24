7373a54d9f1a247c68d546b25a048746505642f2
tan.huynh <tan.huynh@onpoint.vn>
2022-08-24 09:51:36 +0700

    Update components

--- lib/core/components/sized_box_widget.dart
+++ lib/core/components/sized_box_widget.dart
@@ -5,4 +5,5 @@ class SizedBoxWidget {
   static const h15 = SizedBox(height: 15.0);
 
   static const w10 = SizedBox(width: 10.0);
+  static const w20 = SizedBox(width: 20.0);
 }
--- lib/core/helpers/text_style_manager.dart
+++ lib/core/helpers/text_style_manager.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class TextStyleManager {
-  static const smallText = TextStyle(fontSize: 12.0);
-  static const normalText = TextStyle(fontSize: 14.0);
-  static const largeText = TextStyle(fontSize: 16.0);
-  static const extraLargeText = TextStyle(fontSize: 18.0);
+  static const smallText = TextStyle(
+    fontSize: 12.0,
+    color: Colors.black,
+  );
+  static const normalText = TextStyle(
+    fontSize: 14.0,
+    color: Colors.black,
+  );
+  static const largeText = TextStyle(
+    fontSize: 16.0,
+    color: Colors.black,
+  );
+  static const extraLargeText = TextStyle(
+    fontSize: 18.0,
+    color: Colors.black,
+  );
 }
