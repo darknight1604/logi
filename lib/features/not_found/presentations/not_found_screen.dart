8f7721e1856f33cebae5b20140e135fbab65130a
tan.huynh <tan.huynh@onpoint.vn>
2022-08-25 16:09:03 +0700

    Update UI NotFound screen

--- lib/features/not_found/presentations/not_found_screen.dart
+++ lib/features/not_found/presentations/not_found_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:logi/core/helpers/text_style_manager.dart';
+import 'package:logi/gen/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class NotFoundScreen extends StatelessWidget {
   const NotFoundScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('not-found'),
+        child: Text(
+          LocaleKeys.errorMessageNotFound.tr(),
+          style: TextStyleManager.largeText,
+        ),
       ),
     );
   }
